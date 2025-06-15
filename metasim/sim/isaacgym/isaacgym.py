40293e2066333392ee3dfc3f5ac4805146e1ab28
wyzhgxy <2200012805@stu.pku.edu.cn>
2025-06-15 15:32:56 +0800

    [fix] import sensor state

--- metasim/sim/isaacgym/isaacgym.py
+++ metasim/sim/isaacgym/isaacgym.py
@@ -28,7 +28,7 @@ from metasim.queries.base import BaseQueryType
 from metasim.sim import BaseSimHandler
 from metasim.types import Action, DictEnvState
 from metasim.utils.dict import class_to_dict
-from metasim.utils.state import CameraState, ObjectState, RobotState, TensorState
+from metasim.utils.state import CameraState, ObjectState, RobotState, TensorState, SensorState
 
 
 class IsaacgymHandler(BaseSimHandler):

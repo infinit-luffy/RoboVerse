d068cc39fcca9490ba93ab5b40262da2fecf5182
geng-haoran <ghr@berkeley.edu>
2025-08-25 00:47:46 +0000

    [add] hand over for dexbench

--- get_started/2_add_new_robot.py
+++ get_started/2_add_new_robot.py
@@ -11,6 +11,8 @@ except ImportError:
 
 import os
 
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+
 import rootutils
 import torch
 import tyro
@@ -19,6 +21,8 @@ from rich.logging import RichHandler
 
 rootutils.setup_root(__file__, pythonpath=True)
 log.configure(handlers=[{"sink": RichHandler(), "format": "{message}"}])
+
+import time
 
 from metasim.constants import PhysicStateType
 from metasim.scenario.cameras import PinholeCameraCfg
@@ -167,14 +171,13 @@ scenario = ScenarioCfg(
 )
 
 # add cameras
-scenario.cameras = [PinholeCameraCfg(width=1024, height=1024, pos=(1.5, -1.5, 1.5), look_at=(0.0, 0.0, 0.0))]
+scenario.cameras = [PinholeCameraCfg(name="camera_0", pos=(2.0, -1.0, 1.49), look_at=(0.0, -0.5, 0.89))]
 
 # add objects
 scenario.objects = [
-    PrimitiveCubeCfg(
+    RigidObjCfg(
         name="cube",
-        size=(0.1, 0.1, 0.1),
-        color=[1.0, 0.0, 0.0],
+        scale=(1, 1, 1),
         physics=PhysicStateType.RIGIDBODY,
     ),
     PrimitiveSphereCfg(
@@ -208,83 +211,171 @@ init_states = [
     {
         "objects": {
             "cube": {
-                "pos": torch.tensor([0.3, -0.2, 0.05]),
+                "pos": torch.tensor([0.0, -0.37, 0.86]),
                 "rot": torch.tensor([1.0, 0.0, 0.0, 0.0]),
-            },
-            "sphere": {
-                "pos": torch.tensor([0.4, -0.6, 0.05]),
-                "rot": torch.tensor([1.0, 0.0, 0.0, 0.0]),
-            },
-            "bbq_sauce": {
-                "pos": torch.tensor([0.7, -0.3, 0.14]),
-                "rot": torch.tensor([1.0, 0.0, 0.0, 0.0]),
-            },
-            "box_base": {
-                "pos": torch.tensor([0.5, 0.2, 0.1]),
-                "rot": torch.tensor([0.0, 0.7071, 0.0, 0.7071]),
-                "dof_pos": {"box_joint": 0.0},
             },
         },
         "robots": {
-            "new_robot_h1": {
-                "pos": torch.tensor([0.0, 0.0, 1.0]),
-                "rot": torch.tensor([1.0, 0.0, 0.0, 0.0]),
+            "franka_shadow_left": {
+                "pos": torch.tensor([0.0, -1.336, 0.0]),
+                "rot": torch.tensor([0.7071, 0, 0, 0.7071]),
                 "dof_pos": {
-                    "left_hip_yaw": 0.0,
-                    "left_hip_roll": 0.0,
-                    "left_hip_pitch": -0.4,
-                    "left_knee": 0.8,
-                    "left_ankle": -0.4,
-                    "right_hip_yaw": 0.0,
-                    "right_hip_roll": 0.0,
-                    "right_hip_pitch": -0.4,
-                    "right_knee": 0.8,
-                    "right_ankle": -0.4,
-                    "torso": 0.0,
-                    "left_shoulder_pitch": 0.0,
-                    "left_shoulder_roll": 0.0,
-                    "left_shoulder_yaw": 0.0,
-                    "left_elbow": 0.0,
-                    "right_shoulder_pitch": 0.0,
-                    "right_shoulder_roll": 0.0,
-                    "right_shoulder_yaw": 0.0,
-                    "right_elbow": 0.0,
+                    "panda_joint1": 0.0,
+                    "panda_joint2": -0.785398,
+                    "panda_joint3": 0.0,
+                    "panda_joint4": -2.356194,
+                    "panda_joint5": 0.0,
+                    "panda_joint6": 3.1415926,
+                    "panda_joint7": -2.356194,
+                    "WRJ2": 0.0,
+                    "WRJ1": 0.0,
+                    "FFJ4": 0.0,
+                    "FFJ3": 0.0,
+                    "FFJ2": 0.0,
+                    "FFJ1": 0.0,
+                    "MFJ4": 0.0,
+                    "MFJ3": 0.0,
+                    "MFJ2": 0.0,
+                    "MFJ1": 0.0,
+                    "RFJ4": 0.0,
+                    "RFJ3": 0.0,
+                    "RFJ2": 0.0,
+                    "RFJ1": 0.0,
+                    "LFJ5": 0.0,
+                    "LFJ4": 0.0,
+                    "LFJ3": 0.0,
+                    "LFJ2": 0.0,
+                    "LFJ1": 0.0,
+                    "THJ5": 0.0,
+                    "THJ4": 0.0,
+                    "THJ3": 0.0,
+                    "THJ2": 0.0,
+                    "THJ1": 0.0,
+                },
+            },
+            "franka_shadow_right": {
+                "pos": torch.tensor([0.0, 0.336, 0.0]),
+                "rot": torch.tensor([0.7071, 0, 0, -0.7071]),
+                "dof_pos": {
+                    "panda_joint1": 0.0,
+                    "panda_joint2": -0.785398,
+                    "panda_joint3": 0.0,
+                    "panda_joint4": -2.356194,
+                    "panda_joint5": 0.0,
+                    "panda_joint6": 3.1415928,
+                    "panda_joint7": -2.356194,
+                    "WRJ2": 0.0,
+                    "WRJ1": 0.0,
+                    "FFJ4": 0.0,
+                    "FFJ3": 0.0,
+                    "FFJ2": 0.0,
+                    "FFJ1": 0.0,
+                    "MFJ4": 0.0,
+                    "MFJ3": 0.0,
+                    "MFJ2": 0.0,
+                    "MFJ1": 0.0,
+                    "RFJ4": 0.0,
+                    "RFJ3": 0.0,
+                    "RFJ2": 0.0,
+                    "RFJ1": 0.0,
+                    "LFJ5": 0.0,
+                    "LFJ4": 0.0,
+                    "LFJ3": 0.0,
+                    "LFJ2": 0.0,
+                    "LFJ1": 0.0,
+                    "THJ5": 0.0,
+                    "THJ4": 0.0,
+                    "THJ3": 0.0,
+                    "THJ2": 0.0,
+                    "THJ1": 0.0,
                 },
             },
         },
     }
 ]
-handler.set_states(init_states)
-obs = handler.get_states(mode="tensor")
+num_robo_dof = {}
+for robot in scenario.robots:
+    num_robo_dof[robot.name] = robot.num_joints
+dof_num = 0
+for robot in scenario.robots:
+    dof_num += num_robo_dof[robot.name]
+joint_low_limits = {}
+joint_high_limits = {}
+for robot in scenario.robots:
+    joint_low_limits[robot.name] = torch.zeros(num_robo_dof[robot.name], device="cuda:0")
+    joint_high_limits[robot.name] = torch.zeros(num_robo_dof[robot.name], device="cuda:0")
+    for i, joint_name in enumerate(robot.joint_limits.keys()):
+        if robot.actuators[joint_name].fully_actuated:
+            joint_low_limits[robot.name][i] = robot.joint_limits[joint_name][0]
+            joint_high_limits[robot.name][i] = robot.joint_limits[joint_name][1]
+            i += 1
+obs, extras = env.reset(states=init_states)
+for robot in scenario.robots:
+    robot.update_state(obs)
 os.makedirs("get_started/output", exist_ok=True)
 
 
 ## Main loop
 obs_saver = ObsSaver(video_path=f"get_started/output/2_add_new_robot_{args.sim}.mp4")
 obs_saver.add(obs)
-
+start_time = time.time()
 step = 0
-robot = scenario.robots[0]
 for _ in range(100):
     log.debug(f"Step {step}")
-    actions = [
-        {
-            robot.name: {
-                "dof_pos_target": {
-                    joint_name: (
-                        torch.rand(1).item() * (robot.joint_limits[joint_name][1] - robot.joint_limits[joint_name][0])
-                        + robot.joint_limits[joint_name][0]
-                    )
-                    for joint_name in robot.joint_limits.keys()
-                }
-            }
-        }
-        for _ in range(scenario.num_envs)
-    ]
-    handler.set_dof_targets(actions)
-    handler.simulate()
-    obs = handler.get_states(mode="tensor")
+    # actions = [
+    #     {
+    #         robot.name: {
+    #             "dof_pos_target": {
+    #                 joint_name: (
+    #                     torch.rand(1).item() * (robot.joint_limits[joint_name][1] - robot.joint_limits[joint_name][0])
+    #                     + robot.joint_limits[joint_name][0]
+    #                 )
+    #                 for joint_name in robot.joint_limits.keys()
+    #                 if robot.actuators[joint_name].fully_actuated
+    #             }
+    #         }
+    #         for robot in scenario.robots
+    #     }
+    #     for _ in range(scenario.num_envs)
+    # ]
+    left_pos_err = torch.zeros((args.num_envs, 3), device="cuda:0")
+    left_pos_err[:, 1] = 0.0
+    left_pos_err[:, 2] = 0.0
+    left_rot_err = torch.zeros((args.num_envs, 3), device="cuda:0")
+    left_dpose = torch.cat([left_pos_err, left_rot_err], -1).unsqueeze(-1)
+    left_targets = scenario.robots[0].control_arm_ik(left_dpose, args.num_envs, "cuda:0")
+
+    right_pos_err = torch.zeros((args.num_envs, 3), device="cuda:0")
+    right_pos_err[:, 1] = 0.0
+    right_pos_err[:, 2] = 0.0
+    right_rot_err = torch.zeros((args.num_envs, 3), device="cuda:0")
+    right_dpose = torch.cat([right_pos_err, right_rot_err], -1).unsqueeze(-1)
+    right_targets = scenario.robots[1].control_arm_ik(right_dpose, args.num_envs, "cuda:0")
+
+    left_ft_pos_err = torch.zeros((args.num_envs, 5, 3), device="cuda:0")
+    left_ft_pos_err[..., 1] = 0.01
+    right_ft_pos_err = torch.zeros((args.num_envs, 5, 3), device="cuda:0")
+    right_ft_pos_err[..., 1] = -0.01
+    left_ft_rot_err = torch.zeros((args.num_envs, 5, 3), device="cuda:0")
+    right_ft_rot_err = torch.zeros((args.num_envs, 5, 3), device="cuda:0")
+    left_dof_pos = scenario.robots[0].control_hand_ik(left_ft_pos_err, left_ft_rot_err)
+    right_dof_pos = scenario.robots[1].control_hand_ik(right_ft_pos_err, right_ft_rot_err)
+
+    actions = torch.zeros((args.num_envs, dof_num), device="cuda:0")
+    num_dof = 0
+    for robot in scenario.robots:
+        actions[:, num_dof : num_dof + 7] = left_targets if robot.name == "franka_shadow_left" else right_targets
+        actions[:, num_dof + 7 : num_dof + num_robo_dof[robot.name]] = (
+            left_dof_pos if robot.name == "franka_shadow_left" else right_dof_pos
+        )
+        num_dof += num_robo_dof[robot.name]
+    obs, reward, success, time_out, extras = env.step(actions)
+    for robot in scenario.robots:
+        robot.update_state(obs)
     obs_saver.add(obs)
     step += 1
+    if step % 10 == 0:
+        log.info(f"Step {step}, Time Elapsed: {time.time() - start_time:.2f} seconds")
+        start_time = time.time()
 
 obs_saver.save()
